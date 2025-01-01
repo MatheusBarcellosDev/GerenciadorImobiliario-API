f9e28fb6849004cd7320f0a378feb8f08d8b820d
MatheusBarcellosDev <matheusbarcellos61@gmail.com>
2025-01-01 20:41:28 -0300

    Adiciona AppDbContext e mapeamento da entidade User

---
+++ GerenciadorImobiliario-API/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using GerenciadorImobiliario_API.Data.Mappings;
+using GerenciadorImobiliario_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GerenciadorImobiliario_API.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserMap());
+        }
+    }
+}
