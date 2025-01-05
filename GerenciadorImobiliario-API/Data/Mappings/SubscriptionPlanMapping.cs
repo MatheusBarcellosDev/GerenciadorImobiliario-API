0df4f7b75fdc167e1e3cdf0414e7538e50987c41
MatheusBarcellosDev <matheusbarcellos61@gmail.com>
2025-01-05 09:46:02 -0300

    Adiciona mapeamento para SubscriptionPlan

---
+++ GerenciadorImobiliario-API/Data/Mappings/SubscriptionPlanMapping.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using GerenciadorImobiliario_API.Models;
+
+namespace GerenciadorImobiliario_API.Data.Mappings
+{
+    public class SubscriptionPlanMapping : IEntityTypeConfiguration<SubscriptionPlan>
+    {
+        public void Configure(EntityTypeBuilder<SubscriptionPlan> builder)
+        {
+            builder.ToTable("SubscriptionPlan");
+            builder.HasKey(sp => sp.Id);
+
+            builder.Property(sp => sp.Name).IsRequired().HasMaxLength(100);
+            builder.Property(sp => sp.Price).IsRequired();
+            builder.Property(sp => sp.Features).IsRequired().HasConversion(
+                v => string.Join(";", v),
+                v => v.Split(";", StringSplitOptions.RemoveEmptyEntries)
+            );
+        }
+    }
+}
