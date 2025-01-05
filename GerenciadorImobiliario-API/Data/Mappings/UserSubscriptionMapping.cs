37308bee79f3fc73522ce2e6a45475b638fb7e87
MatheusBarcellosDev <matheusbarcellos61@gmail.com>
2025-01-05 09:46:43 -0300

    Adiciona mapeamento para UserSubscription

---
+++ GerenciadorImobiliario-API/Data/Mappings/UserSubscriptionMapping.cs
@@ -0,0 +1,28 @@
+﻿using GerenciadorImobiliario_API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GerenciadorImobiliario_API.Data.Mappings
+{
+    public class UserSubscriptionMapping : IEntityTypeConfiguration<UserSubscription>
+    {
+        public void Configure(EntityTypeBuilder<UserSubscription> builder)
+        {
+            builder.ToTable("UserSubscription");
+            builder.HasKey(us => us.Id);
+
+            builder.Property(us => us.StartDate).IsRequired();
+            builder.Property(us => us.IsActive).IsRequired();
+
+            builder.HasOne(us => us.User)
+                   .WithMany()
+                   .HasForeignKey(us => us.UserId)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(us => us.SubscriptionPlan)
+                   .WithMany()
+                   .HasForeignKey(us => us.SubscriptionPlanId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
