4ee9bd95ad25532e5833ef5e898a7de08b839bc6
MatheusBarcellosDev <matheusbarcellos61@gmail.com>
2025-01-05 09:48:38 -0300

    Adiciona classe SubscriptionPlan e novos using statements

---
+++ GerenciadorImobiliario-API/Models/SubscriptionPlan.cs
@@ -0,0 +1,15 @@
+﻿using GerenciadorImobiliario_API.Enums;
+using System.Text.Json.Serialization;
+
+namespace GerenciadorImobiliario_API.Models
+{
+    public class SubscriptionPlan
+    {
+        public long Id { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public ESubscriptionPlan Name { get; set; }
+        public decimal Price { get; set; }
+        public string[] Features { get; set; } = Array.Empty<string>();
+    }
+}
