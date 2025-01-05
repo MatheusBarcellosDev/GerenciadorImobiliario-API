d1cd523a4b146452110f51ed8d8476d4df97e2ce
MatheusBarcellosDev <matheusbarcellos61@gmail.com>
2025-01-05 09:49:31 -0300

    Adiciona serviço de envio de e-mails

---
+++ GerenciadorImobiliario-API/Services/EmailService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Net.Mail;
+
+namespace Blog.Services
+{
+    public class EmailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public bool Send
+            (
+                string toName,
+                string toEmail,
+                string subject,
+                string body,
+                string fromName = "Matheus",
+                string fromEmail = "emailteste@lorenalorenzoimoveis.com.br"
+            )
+        {
+            var smtpClient = new SmtpClient(
+                _configuration["Smtp:Host"],
+                int.Parse(_configuration["Smtp:Port"])
+            );
+
+            smtpClient.Credentials = new NetworkCredential(
+                _configuration["Smtp:Username"],
+                _configuration["Smtp:Password"]
+            );
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.EnableSsl = true;
+
+            var mail = new MailMessage
+            {
+                From = new MailAddress(fromEmail, fromName),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            };
+            mail.To.Add(new MailAddress(toEmail, toName));
+
+            try
+            {
+                smtpClient.Send(mail);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
