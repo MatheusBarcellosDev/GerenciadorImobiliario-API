39f55d62e9cc0f6866c2282cf4520b6f46fd4e86
MatheusBarcellosDev <matheusbarcellos61@gmail.com>
2025-01-05 09:51:21 -0300

    Adiciona classe Configuration para configurações da aplicação

---
+++ GerenciadorImobiliario-API/Configuration.cs
@@ -0,0 +1,16 @@
+﻿namespace GerenciadorImobiliario_API
+{
+    public class Configuration
+    {
+        public static string JwtKey { get; set; } = "ZjQ3YWMxMGItNThjYy00MzcyLWE1NjctMGUwMmIyYzNkNDc5";
+        public static SmtpConfigurarion Smtp { get; set; } = new SmtpConfigurarion();
+
+        public class SmtpConfigurarion
+        {
+            public string Host { get; set; }
+            public int Port { get; set; } = 25;
+            public string UserName { get; set; }
+            public string Password { get; set; }
+        }
+    }
+}
