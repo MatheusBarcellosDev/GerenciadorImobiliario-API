069d8016bd5128073b7e5325285a3c6a4509aa5b
MatheusBarcellosDev <matheusbarcellos61@gmail.com>
2025-01-06 19:53:20 -0300

    Adiciona classe Lead ao modelo de gerenciamento imobiliário

---
+++ GerenciadorImobiliario-API/Models/Lead.cs
@@ -0,0 +1,23 @@
+﻿namespace GerenciadorImobiliario_API.Models
+{
+    public class Lead
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Telephone { get; set; } = string.Empty;
+        public string InitialNotes { get; set; } = string.Empty;
+        public DateTime DateContacted { get; set; }
+        public long LeadStatusId { get; set; }
+        public LeadStatus LeadStatus { get; set; } = null!;
+        public long CurrentPipelineStageId { get; set; }
+        public PipelineStage CurrentPipelineStage { get; set; } = null!;
+        public long UserId { get; set; }
+        public User User { get; set; } = null!;
+        public long? ClientId { get; set; }
+        public Client? Client { get; set; }
+        public DateTime? LastInteractionDate { get; set; }
+
+        public bool IsActive { get; set; } = true;
+    }
+}
