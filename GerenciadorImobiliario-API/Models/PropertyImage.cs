be13cf298fa79127ee402c6643c6758abaf5fd63
MatheusBarcellosDev <matheusbarcellos61@gmail.com>
2025-01-06 19:54:36 -0300

    Adiciona classe PropertyImage ao modelo

---
+++ GerenciadorImobiliario-API/Models/PropertyImage.cs
@@ -0,0 +1,10 @@
+﻿namespace GerenciadorImobiliario_API.Models
+{
+    public class PropertyImage
+    {
+        public long Id { get; set; }
+        public string Url { get; set; } = string.Empty;
+        public long PropertyId { get; set; }
+        public Property Property { get; set; } = null!;
+    }
+}
