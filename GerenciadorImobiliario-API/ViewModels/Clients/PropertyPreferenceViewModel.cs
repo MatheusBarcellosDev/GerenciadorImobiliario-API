db759d91da00b638989d9d940e1b5f35547faab1
MatheusBarcellosDev <matheusbarcellos61@gmail.com>
2025-01-06 19:55:58 -0300

    Adiciona PropertyPreferenceViewModel

---
+++ GerenciadorImobiliario-API/ViewModels/Clients/PropertyPreferenceViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace GerenciadorImobiliario_API.ViewModels.Clients
+{
+    public class PropertyPreferenceViewModel
+    {
+        public string Type { get; set; }
+        public string Location { get; set; }
+        public int MinBedrooms { get; set; }
+        public int MaxBedrooms { get; set; }
+        public decimal MinPrice { get; set; }
+        public decimal MaxPrice { get; set; }
+        public string AdditionalPreferences { get; set; }
+    }
+}
