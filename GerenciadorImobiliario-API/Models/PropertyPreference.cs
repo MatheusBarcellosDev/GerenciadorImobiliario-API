369e9393f44385f578bab3604a3d5820eb613088
MatheusBarcellosDev <matheusbarcellos61@gmail.com>
2025-01-06 19:54:53 -0300

    Adiciona classe PropertyPreference em Models

---
+++ GerenciadorImobiliario-API/Models/PropertyPreference.cs
@@ -0,0 +1,16 @@
+﻿namespace GerenciadorImobiliario_API.Models
+{
+    public class PropertyPreference
+    {
+        public long Id { get; set; }
+        public long ClientId { get; set; }
+        public Client Client { get; set; }
+        public string Type { get; set; }
+        public string Location { get; set; }
+        public int MinBedrooms { get; set; }
+        public int MaxBedrooms { get; set; }
+        public decimal MinPrice { get; set; }
+        public decimal MaxPrice { get; set; }
+        public string AdditionalPreferences { get; set; }
+    }
+}
