71183447d7b71732e609bc2a9cde3682307f5a40
MatheusBarcellosDev <matheusbarcellos61@gmail.com>
2025-01-06 19:54:05 -0300

    Adiciona classe Property ao modelo de imóveis

---
+++ GerenciadorImobiliario-API/Models/Property.cs
@@ -0,0 +1,35 @@
+﻿namespace GerenciadorImobiliario_API.Models
+{
+    public class Property
+    {
+        public long Id { get; set; }
+        public long? AddressId { get; set; }
+        public Address? Address { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public long OwnerId { get; set; }
+        public Client Owner { get; set; } = null!;
+        public string? Type { get; set; } 
+        public int NumberOfBedrooms { get; set; }
+        public int NumberOfBathrooms { get; set; }
+        public int NumberOfParkingSpaces { get; set; }
+        public double TotalArea { get; set; }
+        public double UsableArea { get; set; }
+        public bool IsFurnished { get; set; }
+        public bool HasBuiltInWardrobes { get; set; }
+        public bool HasPool { get; set; }
+        public bool HasBarbecueGrill { get; set; }
+        public bool HasBalcony { get; set; }
+        public bool HasGarden { get; set; }
+        public decimal? RentPrice { get; set; }
+        public decimal? CondominiumFee { get; set; }
+        public decimal? Iptu { get; set; }
+        public string? Status { get; set; } 
+        public List<PropertyImage> Images { get; set; } = new List<PropertyImage>();
+        public List<PropertyDocument> Documents { get; set; } = new List<PropertyDocument>();
+        public long UserId { get; set; } 
+        public User User { get; set; } = null!;
+        public List<ClientPropertyInterest> ClientPropertyInterests { get; set; } = new List<ClientPropertyInterest>();
+
+    }
+}
