96121bbe13bb5ffedc0967262faac3f89af6a3e2
MatheusBarcellosDev <matheusbarcellos61@gmail.com>
2025-01-06 19:56:47 -0300

    Adiciona PropertyViewModel com várias propriedades

---
+++ GerenciadorImobiliario-API/ViewModels/Property/PropertyViewModel.cs
@@ -0,0 +1,33 @@
+﻿using GerenciadorImobiliario_API.Models;
+using GerenciadorImobiliario_API.ViewModels.Property;
+
+namespace GerenciadorImobiliario_API.ViewModels
+{
+    public class PropertyViewModel
+    {
+        public long Id { get; set; }
+        public Address? Address { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public long OwnerId { get; set; }
+        public OwnerViewModel Owner { get; set; } = null!;
+        public string? Type { get; set; }
+        public int NumberOfBedrooms { get; set; }
+        public int NumberOfBathrooms { get; set; }
+        public int NumberOfParkingSpaces { get; set; }
+        public double TotalArea { get; set; }
+        public double UsableArea { get; set; }
+        public bool IsFurnished { get; set; }
+        public bool HasBuiltInWardrobes { get; set; }
+        public bool HasPool { get; set; }
+        public bool HasBarbecueGrill { get; set; }
+        public bool HasBalcony { get; set; }
+        public bool HasGarden { get; set; }
+        public decimal? RentPrice { get; set; }
+        public decimal? CondominiumFee { get; set; }
+        public decimal? Iptu { get; set; }
+        public string? Status { get; set; }
+        public long UserId { get; set; }
+    }
+}
+
