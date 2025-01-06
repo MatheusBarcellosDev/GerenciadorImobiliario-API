d6f3fb57caca934d70a12acfa19ed20e2019bc0c
MatheusBarcellosDev <matheusbarcellos61@gmail.com>
2025-01-06 19:55:04 -0300

    Refatoração de propriedades e adição de relacionamentos

--- GerenciadorImobiliario-API/Models/User.cs
+++ GerenciadorImobiliario-API/Models/User.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.Net;
 
 namespace GerenciadorImobiliario_API.Models
 {
@@ -10,38 +11,39 @@ namespace GerenciadorImobiliario_API.Models
         public string Slug { get; set; } = string.Empty;
 
         // Informações de Contato
-        public string? PhoneNumber { get; set; }
-        public string? Address { get; set; }
-        public string? City { get; set; }
-        public string? State { get; set; }
-        public string? PostalCode { get; set; }
+        public new string? PhoneNumber { get; set; } 
+        public long? AddressId { get; set; }
+        public Address? Address { get; set; }
 
         // Informações Profissionais
-        public string? Creci { get; set; } // Número de registro no CRECI
-        public string? Specialties { get; set; } // Áreas de especialidade
-        public int YearsOfExperience { get; set; } // Anos de experiência
+        public string? Creci { get; set; }
+        public string? Specialties { get; set; }
+        public int YearsOfExperience { get; set; }
 
         // Informações Adicionais
-        public string? Description { get; set; } // Breve descrição ou biografia
-        public string? LinkedIn { get; set; } // Link para o perfil do LinkedIn
-        public string? Instagram { get; set; } // Link para o perfil do Instagram
+        public string? Description { get; set; }
+        public string? LinkedIn { get; set; }
+        public string? Instagram { get; set; }
 
         // Assinatura
-        public long? SubscriptionPlanId { get; set; } // Chave estrangeira para SubscriptionPlan
-        public SubscriptionPlan? SubscriptionPlan { get; set; } // Propriedade de navegação
+        public long? SubscriptionPlanId { get; set; }
+        public SubscriptionPlan? SubscriptionPlan { get; set; }
         public DateTime? SubscriptionStartDate { get; set; }
         public DateTime? SubscriptionEndDate { get; set; }
-        public bool IsActive { get; set; } = true; // Indica se a assinatura está ativa
+        public bool IsActive { get; set; } = true;
 
         // Período de Teste
         public DateTime? TrialStartDate { get; set; }
         public DateTime? TrialEndDate { get; set; }
-
-        // Propriedade adicional para controlar se o período de teste foi usado
         public bool TrialUsed { get; set; } = false;
 
         // Propriedades de Auditoria
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime? UpdatedAt { get; set; }
+
+        // Relacionamentos
+        public ICollection<Lead> Leads { get; set; } = new List<Lead>();
+        public ICollection<Client> Clients { get; set; } = new List<Client>();
     }
+
 }
