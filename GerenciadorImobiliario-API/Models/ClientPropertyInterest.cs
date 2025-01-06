bfde9a4f0f9d8ce0a321e37d0fe3d6c2d6100234
MatheusBarcellosDev <matheusbarcellos61@gmail.com>
2025-01-06 19:53:03 -0300

    Adiciona classe ClientPropertyInterest

---
+++ GerenciadorImobiliario-API/Models/ClientPropertyInterest.cs
@@ -0,0 +1,11 @@
+﻿namespace GerenciadorImobiliario_API.Models
+{
+    public class ClientPropertyInterest
+    {
+        public long ClientId { get; set; }
+        public Client Client { get; set; } = null!;
+        public long PropertyId { get; set; }
+        public Property Property { get; set; } = null!;
+        public DateTime InterestDate { get; set; }
+    }
+}
