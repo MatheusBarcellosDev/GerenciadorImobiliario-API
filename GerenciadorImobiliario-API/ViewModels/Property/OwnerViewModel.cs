213952f5da860b5b3d5f623b84fb67f604d7e352
MatheusBarcellosDev <matheusbarcellos61@gmail.com>
2025-01-06 19:56:35 -0300

    Adiciona classe OwnerViewModel

---
+++ GerenciadorImobiliario-API/ViewModels/Property/OwnerViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace GerenciadorImobiliario_API.ViewModels.Property
+{
+    public class OwnerViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
